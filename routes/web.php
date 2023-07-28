ebf3dea4e659f0adc7f2ab5a520f5f28f96881c2
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-07-28 21:04:29 +0700

    feats: test api request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/make-api-request', function () {
+    $url = 'http://34.101.154.14:8175/hackathon/user/auth/create';
+    $data = [
+        "ktpId" => "4746585764531234",
+        "userName" => "Ayu",
+        "phoneNumber" => "81234567890",
+        "loginPassword" => "12345678",
+        "birthDate" => "11101998",
+        "gender" => 1,
+        "email" => "ayu@gmail.com"
+    ];
+
+    $response = Http::post($url, $data);
+    // dd($response);
+
+    // Handle the response
+    $responseData = $response->json();
+    dd($response->status());
+    // Do something with $responseData
 });
