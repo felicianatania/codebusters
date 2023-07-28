bee519d30ab6410041bc43cdc7ffd0870987cca7
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-07-28 22:54:01 +0700

    feats: add tailwind theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,35 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        black: '#222222',
+        gray: '#555555',
+        white: '#FFFFFF',
+        darkorange: '#F77744',
+        lightorange: '#FCBD00',
+        yellow: '#FEDC18',
+        lightyellow: '#FFFBE8',
+        darkblue: '#04A8F0',
+        lightblue: '#36B9F3',
+      },
+      fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        headline: '3rem',
+        title: '1.875rem',
+        heading: '1.3125rem',
+        subheading: '1.125rem',
+        name: '1rem',
+        subname: '0.875rem',
+        light: '0.8rem',
+
+      },
+      borderRadius: {
+        'default': '5px',
+      }
+    },
   },
   plugins: [],
 }
