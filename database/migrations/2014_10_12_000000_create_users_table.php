94c0170038b3e4758c5f5a16886ac8e61cf355dd
felicianatania <felicianatania0@gmail.com>
2023-07-28 19:24:05 +0700

    feat: add table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('ktpId');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phoneNumber');
+            $table->string('userName')->unique();
+            $table->string('loginPassword');
+            $table->string('confirmationPassword');
+            $table->integer('gender')->default('1');
+            $table->date('birthDate');
             $table->rememberToken();
             $table->timestamps();
         });
