af87aa3987cddd531c52dc4bd6b4fc9fa6b24e88
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-07-29 16:18:28 +0700

    feats: fix login page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
     public function login() {
-        return Inertia::render('login');
+        return Inertia::render('Auth/Login');
     }
 
     public function register() {
-        return Inertia::render('register');
+        return Inertia::render('Auth/Register');
     }
 
     public function validateLogin() {
