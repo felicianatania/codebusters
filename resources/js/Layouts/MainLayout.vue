7d5ae9733a78661836bdcad9cef50e8b950596ce
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-07-29 09:57:55 +0700

    feats: merge branches

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -13,37 +13,47 @@ import Exit from 'vue-material-design-icons/ExittoApp.vue';
 
 <template>
     <div id="MainLayout" class="w-full h-screen">
-        <div id="SideNav" class="text-lg fixed h-full bg-yellow-300 xl:w-[240px] w-[80px] md:block hidden border-r border-r-gray-300">
+        <div id="SideNav" class="text-lg fixed h-full bg-yellow-300 xl:w-[260px] w-[80px] md:block hidden bg-yellow">
 
             <Link href="/">
                 <img src="" alt="logo" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
-                <img src="" alt="text logo" class="xl:block hidden w-[120px] mt-10 ml-10 mb-10 cursor-pointer">
+                <img src="" alt="text logo" class="xl:block hidden w-[120px] mt-12 ml-12 mb-10 cursor-pointer">
             </Link>
-            <div class="ml-10 text-gray-600 font-medium font-poppins">
+            <div class="ml-12 text-gray-600 font-medium font-poppins">
                 <div class="flex items-center mb-7">
                     <Dashboard fillColor="#555555" :size="30" class="cursor-pointer" />
-                    <h1 class="ml-2">Dashboard</h1>
+                    <h1 class="ml-4">Dashboard</h1>
                 </div>
                 <div class="flex items-center mb-7">
                     <HomeOutline fillColor="#555555" :size="30" class="cursor-pointer" />
-                    <h1 class="ml-2">Beli Rumah</h1>
+                    <h1 class="ml-4">Beli Rumah</h1>
                 </div>
                 <div class="flex items-center mb-7">
                     <Car fillColor="#555555" :size="30" class="cursor-pointer" />
-                    <h1 class="ml-2">Beli Mobil</h1>
+                    <h1 class="ml-4">Beli Mobil</h1>
                 </div>
                 <div class="flex items-center w-10">
                     <Predict fillColor="#555555" :size="30" class="cursor-pointer" />
-                    <h1 class="ml-2">Memprediksi Pengeluaran</h1>
+                    <h1 class="ml-4">Memprediksi Pengeluaran</h1>
                 </div>
             </div>
 
 
-            <div class="flex items-center absolute bottom-10 px-10 w-full">
+            <div class="flex items-center absolute bottom-10 ml-12 w-full">
                 <Exit fillColor="#555555" :size="30" class="cursor-pointer" />
-                <h1 class="ml-2 text-gray-600 font-medium">Log out</h1>
+                <h1 class="ml-4 text-gray-600 font-medium">Log out</h1>
             </div>
         </div>
 
+        <div class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[50px] lg:pl-[100px] overflow-auto">
+            <div
+                class="mx-auto md:pt-6 mt-6"
+                :class="$page.url === '/' ? 'lg:w-8/12 w-full' : 'max-w-[1200px]'"
+            >
+                <main>
+                    <slot></slot>
+                </main>
+            </div>
+        </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,42 +3,43 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        black: '#222222',
-        gray: '#555555',
-        white: '#FFFFFF',
-        darkorange: '#F77744',
-        lightorange: '#FCBD00',
-        yellow: '#FEDC18',
-        lightyellow: '#FFFBE8',
-        darkblue: '#04A8F0',
-        lightblue: '#36B9F3',
-      },
-      fontFamily: {
-          sans: ['Poppins', 'sans-serif'],
-      },
-      fontSize: {
-        headline: '3rem',
-        title: '1.875rem',
-        heading: '1.3125rem',
-        subheading: '1.125rem',
-        name: '1rem',
-        subname: '0.875rem',
-        light: '0.8rem',
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
 
-      },
-      borderRadius: {
-        'default': '5px',
-      }
+    theme: {
+        extend: {
+            colors:{
+                black: '#222222',
+                gray: '#555555',
+                white: '#FFFFFF',
+                darkorange: '#F77744',
+                lightorange: '#FCBD00',
+                yellow: '#FEDC18',
+                lightyellow: '#FFFBE8',
+                darkblue: '#04A8F0',
+                lightblue: '#36B9F3',
+            },
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+            fontSize: {
+                headline: '3rem',
+                title: '1.875rem',
+                heading: '1.3125rem',
+                subheading: '1.125rem',
+                name: '1rem',
+                subname: '0.875rem',
+                light: '0.8rem',
+            },
+            borderRadius: {
+                'default': '5px',
+            }
+        },
     },
-  },
-  plugins: [],
-}
 
+    plugins: [forms],
+};
